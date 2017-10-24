514f7412e27103203860bf49a5f866f8acda1455
Matt Lindsay <matt_epilp@hotmail.com>
2017-10-24 12:06:58 +0100

    Deep-diff implemented

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -2,17 +2,31 @@
 
 const fs = require('fs');
 const path = require('path');
-const deepDiff = require('deep-diff');
+const diff = require('deep-diff');
 
 const indexController = function (getDataService) {
     var getData = function (req, res) {
         getDataService.getData(function (err, results) {
             if (err) res.status(500).send(err);
 
+            // New data from H Reg database in JSON format.
             var newData = results.recordset;
-            var previousData = JSON.parse(fs.readFileSync(__dirname + '/../data/previousData.json'));
-            // returning an object, not json
-            res.send(previousData);
+            // Data from last program execution stored in JSON format.
+            var storedData = JSON.parse(fs.readFileSync(__dirname + '/../data/previousData.json'));
+            // Comparison of new and stored JSON datasets.
+            var differences = diff(storedData.sort(), newData.sort());
+            // Newly added JSON objects.
+            var recordsToInsert = [];
+            differences.forEach(function (item) {
+                if (item.item) {//recordsToInsert.push(item);
+                    if (item.item.kind === 'N') {
+                        console.log(item);
+                        recordsToInsert.push(item.item.rhs);
+                    }
+                }
+            });
+
+            res.send(differences);
         });
     };
 
