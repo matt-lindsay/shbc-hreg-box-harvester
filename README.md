acc9a3e448ffe19e6ac69df0858fe80b295e3e39
Matt Lindsay <matt_epilp@hotmail.com>
2017-11-14 11:07:07 +0000

    First endpoint working

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # shbc-hreg-box-harvester
-Harvest H Reg numbers to create case folders in Box.
+
+### Purpose
+
+- Harvest H Reg numbers and create case folders in Box. Requires something to execute it on a scheduled basis, no more frequently than once an hour.
+- Receive H Task numbers and create case folders in Box.
--- src/services/BoxService.js
+++ src/services/BoxService.js
@@ -12,6 +12,8 @@ var BoxService = function () {
     var publicKeyId = process.env.boxKeyID;
     var publicKeyPassphrase = process.env.boxKeyPassphrase;
     var boxEnterpriseId = process.env.boxEnterpriseId;
+    var boxHousingUser = process.env.boxHousingUser;
+    var boxHousingFolder = process.env.boxHousingFolder;
 
     var createFolders = function (data, cb) {
         var sdk = new Box({
@@ -25,24 +27,20 @@ var BoxService = function () {
         });
 
         var client = sdk.getAppAuthClient('enterprise', boxEnterpriseId);
-        client.asUser('281003709'); // 281003709 me, 1429785787 another user.
-
-        // Housing Option: 22303582137
-        // 02 Housing Documents: 22304818963 ** CREATE FOLDERS HERE **
-        // Dev: 14274812032
+        client.asUser(boxHousingUser);
         
         // Create folders from 'data' array.
         // Sub folders to be created are stored in a JSON file.
         let subFolders = JSON.parse(fs.readFileSync(__dirname + '/../data/subFolderArray.json'));
         
         // need a better way to handle response back to Express app.
-        var results = [];
+        var results = false;
         var timestamp = moment().format('YYYY-MM-DD h:mm:ss a');
 
         data.forEach(function (folderName) {
             console.log('>>> ' + timestamp + ' Record Found: ' + folderName);
             // SEARCH for the existence of a folder.
-            client.search.query(folderName, { type: 'folder', ancestor_folder_ids: '14274812032' }, function (err, queryResponse) {
+            client.search.query(folderName, { type: 'folder', ancestor_folder_ids: boxHousingFolder }, function (err, queryResponse) {
                 if (err) console.log(err);
                 
                 // Obtain the number of folders found matching the search case number.
@@ -51,25 +49,79 @@ var BoxService = function () {
                 // If a folder doesn't exist, CREATE it and it's sub folders.
                 // If the recordCount is 0 then no match is present within Box.
                 if (queryResponse.total_count === 0) {
-                    client.folders.create('14274812032', folderName, function (err, createResponse) {
-                        let createdFolderName = createResponse.name;
-                        let createdFolderId = createResponse.id;
+                    createBoxFolders(client, folderName, subFolders, timestamp);
+                    results = true;
 
-                        console.log('>>> Folder created: ' + createdFolderName);                        
-                        subFolders.forEach(function (subFolderName) {
-                            client.folders.create(createdFolderId, subFolderName, function (err, subFolderCreateResponse) {
-                                if (err) console.log(err);
-                                console.log('>>> ' + timestamp + ' Folder created: ' + subFolderName);
-                            });
-                        });
-                        results.push(createResponse);
+                    // client.folders.create(boxHousingFolder, folderName, function (err, createResponse) {
+                    //     let createdFolderName = createResponse.name;
+                    //     let createdFolderId = createResponse.id;
+
+                    //     console.log('>>> Folder created: ' + createdFolderName);                        
+                    //     subFolders.forEach(function (subFolderName) {
+                    //         client.folders.create(createdFolderId, subFolderName, function (err, subFolderCreateResponse) {
+                    //             if (err) console.log(err);
+                    //             console.log('>>> ' + timestamp + ' Sub Folder created: ' + subFolderName);
+                    //         });
+                    //     });
+                    //     results.push(createResponse);
+                    // });
+                } else { // Else iterate through the returned results and test for a match.
+                    let searchResults = queryResponse.entries;
+                    
+                    matchResults(searchResults, folderName).then(function (response) {
+                        console.log(response);
+                        createBoxFolders(client, folderName, subFolders, timestamp);
+                        results = true;
+                    }, function (error) {
+                        console.log('>>> Folder already exists.');
                     });
-                } else { // Else If a folder does exist, skip that record and move onto the next one.
-                    console.log('>>> ' + timestamp + ' Folder exists: ' + queryResponse.entries[0].name);
                 }
             });
         });
-        cb(null, results);
+        cb(null, { 'Folders created': results });
+    };
+
+    var matchResults = function (searchResults, folderName) {
+        return new Promise(function (resolve, reject) {
+
+            // Test each result returned in searchResults for a match with folderName. If a match is not returned create the folder.
+            let matched = false;
+
+            searchResults.forEach(function (item) {
+                if (item.type === 'folder') {
+                    console.log('>>> Folders found: ' + item.name);
+                    // If a folder does exist, set 'matched' to true.
+                    if (item.name.match(folderName)) {
+                        console.log('>>> ' + timestamp + 'Matched: ' + item.name);
+                        matched = true;
+                    } else {
+                        console.log('>>> Not Matched: ' + item.name);
+                    }
+                }
+            });
+            if (matched === false) {
+                resolve('nomatch');
+            } else if (matched === true) {
+                reject('matched');
+            }
+        });
+    };
+
+    var createBoxFolders = function (client, folderName, subFolders, timestamp) {
+        client.folders.create(boxHousingFolder, folderName, function (err, createFolderResponse) {
+            //console.log(createFolderResponse);
+            let createFolderName = createFolderResponse.name;
+            let createdFolderId = createFolderResponse.id;
+
+            console.log('>>> Folder created: ' + createFolderName);                       
+            
+            subFolders.forEach(function (subFolderName) {
+                client.folders.create(createdFolderId, subFolderName, function (err, secondSubFolderCreateResponse) {
+                    if (err) console.log(err);
+                    console.log('>>> ' + timestamp + ' Sub Folder created: ' + subFolderName);
+                });
+            });
+        });
     };
 
     return {
