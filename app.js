a29c603592baf41f6d42aff071e997f069881c68
Matt Lindsay <matt_epilp@hotmail.com>
2018-02-22 11:22:37 +0000

    Changed apps default port to resolve conflict with other apps.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 var BoxClient = require('./src/resources/box');
 let client = new BoxClient();
